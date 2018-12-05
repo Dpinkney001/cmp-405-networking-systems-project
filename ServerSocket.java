94c27edf4ef0aee1cc0ba6d453949c80acd5b16c
Duvall Pinkney <14938639+theesckey60@users.noreply.github.com>
2018-12-04 20:39:19 -0500

    Add files via upload

--- ServerSocket.java
+++ ServerSocket.java
@@ -1,119 +1,151 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wifi.chat.program;
 
-import java.net.*;
+
+
+import java.io.IOException;
+  import java.net.*;
 import java.util.Scanner;
-import java.util.concurrent.TimeUnit;
-
-
-
-public class ServerSocket   {
-
-	static private InetAddress myAddress = null;
-
-	@SuppressWarnings("resource")
-	public  static void ServerMethod() {
-
-		DatagramSocket inSocket = null;
-		byte[] inBuffer = new byte[100];
-		DatagramPacket inPacket = new DatagramPacket(inBuffer, inBuffer.length);
-
+//import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author DUVALL NOTEBOOK
+ */
+public class ServerSocket extends WifiChatProgram {
+    
+    static private InetAddress myAddress = null;
+    static private InetAddress serverAddress = null;
+    
+    
+           public static void ServerMethod(){
 
 		try {
-			inSocket = new DatagramSocket(64000, myAddress);
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.exit(-1);
+                    
+                    DatagramSocket inSocket;
+                    inSocket = null;
+                    byte[] inBuffer = new byte[256];
+                    DatagramPacket inPacket = new DatagramPacket(inBuffer, inBuffer.length);
+                    
+                    serverAddress = InetAddress.getLocalHost();
+                    try {
+                        inSocket = new DatagramSocket(64000, serverAddress);
+                    } catch (SocketException e) {
+                        System.exit(-1);
+                    }
+                    
+                    do {
+                        for ( int i = 0 ; i < inBuffer.length ; i++ ) {
+                            inBuffer[i] = ' ';
+                        }
+                        
+                        try {
+                            // this thread will block in the receive call
+                            // until a message is received
+                            System.out.println("Waiting for input...");
+                            inSocket.receive(inPacket);
+                        } catch (IOException e) {
+                            System.exit(-1);
+                        }
+                        
+                        String message = new String(inPacket.getData());
+                        System.out.println("Received message = " + message);
+                        
+                    } while(true);
+                } catch (UnknownHostException ex) {
+			Logger.getLogger(ServerSocket.class.getName()).log(Level.SEVERE, null, ex);
 		}
-
-		do {
-			for ( int i = 0 ; i < inBuffer.length ; i++ ) {
-				inBuffer[i] = ' ';
-			}
-
-			try {
-				// this thread will block in the receive call
-				// until a message is received
-				System.out.println("Waiting for input...");
-						inSocket.receive(inPacket);
-			} catch (Exception e) {
-				e.printStackTrace();
-				System.exit(-1);
-			}
-
-			String message = new String(inPacket.getData());
-			System.out.println("Received message = " + message);
-
-		} while(true);
 	}
-
-
-
-
-
-	public static void main(String[] args) {
-
+    /*
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        
+        Scene scene = new Scene(root, 300, 250);
+        
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+*/
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //launch(args);
+        
+       
 		try {
 			myAddress = InetAddress.getLocalHost();
-		} catch (Exception e) {
-			e.printStackTrace();
+		} catch (UnknownHostException e) {
 			System.exit(-1);
 		}
 
 		System.out.println("My Address = " + myAddress.getHostAddress());
-
-		DatagramSocket outSocket = null;
+        
+                
+		DatagramSocket outSocket;
+        outSocket = null;
 
 		try {
 			outSocket = new DatagramSocket(63000, myAddress);
-		} catch (Exception e) {
-			e.printStackTrace();
+		} catch (SocketException e) {
 			System.exit(-1);
 		}
+                        
+                Scanner input = new Scanner(System.in);
 
-		Thread receiveThread = new Thread(new Runnable () {
-			public void run() {
-				ServerMethod();
-			}
-		});
-		receiveThread.setName("Receive Thread");
-		receiveThread.start();
+                String prefix = "Message number ";
+		byte[] outBuffer = new byte[256];
 
-		Scanner scnr = new Scanner(System.in);
-		System.out.println("Start Sending? Press Enter...");
-		scnr.nextLine();
-		scnr.close();
-
-		String prefix = "Message number ";
-		byte[] buffer = new byte[100];
-
-		for ( int i = 1 ; i <= 10 ; i++ ) {
+		for ( int i = 1 ; i <= 1000 ; i++ ) {
+                        System.out.println("enter message here");
 			String message = prefix + i;
-			buffer = message.getBytes();
-
-			try {
-				DatagramPacket packet = new DatagramPacket(buffer, 
-						message.length(),
+                        String data = message + input.nextLine();
+			outBuffer = data.getBytes();
+				DatagramPacket packet = new DatagramPacket(outBuffer, 
+						data.length(),
 						myAddress,
 						64000);
 
-				System.out.println("Sending message = " + message);
-				outSocket.send(packet);
-				TimeUnit.SECONDS.sleep(5);
-			} catch (Exception e) {
-				e.printStackTrace();
-				System.exit(-1);
+				System.out.println("Sending message = " + data);
+                              
+                    try {
+                        outSocket.send(packet);
+                    } catch (IOException ex) {
+                        Logger.getLogger(ServerSocket.class.getName()).log(Level.SEVERE, null, ex);
+                    }
 			}
-		}
+		
+                
+		Thread receiveThread = new Thread(ServerSocket::ServerMethod);
+		receiveThread.setName("Receive Thread");
+		receiveThread.start();
 
-		try {
-			TimeUnit.MINUTES.sleep(1);
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.exit(-1);
-		}
-
-		System.out.println("Main method exiting.... Bye Bye....");
-		System.exit(0);
-	}
+        }
+	
 }
 
+
+
+
+
+    
+    
 
